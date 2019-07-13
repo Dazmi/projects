1ca94b0e9e94b48276aea287b3bb338f654b02e5
Dazmi <36841434+Dazmi@users.noreply.github.com>
2019-07-14 00:32:04 +1000

    deply

--- app.py
+++ app.py
@@ -17,6 +17,7 @@ import os
 app = Flask(__name__)
 CORS(app)
 asxlist = pd.read_csv('ASXListedCompanies.csv',  header=1)
+asxlist.rename(columns={'ASX code': 'Code'}, inplace=True)
 asx200 = pd.read_csv('20190701-asx200.csv',  header=1)
 corr = pd.read_csv('corr.csv',  header=0, index_col=0)
 latest = pd.read_csv('latest_data.csv',  header=0, index_col=0)
@@ -80,13 +81,13 @@ def stats():
         days = 1825
 
     df = df.round({'Adj Close': 3, 'Volume': 0})
-    X_data = df['Date'].dt.strftime('%d/%m/%Y')
+    X_data = df['Date']#.dt.strftime('%d/%m/%Y')
     X_data = X_data.tail(days).tolist()
     Y_data = df['Adj Close'].tail(days)
     Y_data = Y_data.tolist()
     volume = df['Volume'].tail(days).tolist()
 
-    asxindex = asxlist.set_index('ASX code')
+    asxindex = asxlist.set_index('Code')
     company = asxindex.loc[ticker,:]
 
     corr = pd.read_csv('corr.csv',  header=0, index_col=0)
@@ -114,83 +115,74 @@ def stats():
     }
     return jsonify(res)
 
-
-def get_data(ticker):
-    print(ticker)
-    df = web.DataReader(f'{ticker}.ax', 'yahoo')
-    df.to_csv(f'data/{ticker}.csv')
-
-
-@app.route("/update_data")
-def thread_get():
-    start = time.time()
-    pool = ThreadPool(8)
-    results = pool.map(get_data, asxlist['ASX code'])
-    pool.close()
-    pool.join()
-    print(time.time() - start)
-    return jsonify({'message':'Complete'})
-
-def latest_data(ticker):
-    #df = pd.read_csv(f'data/{ticker}.csv',  header=0)
-    df = web.DataReader(f'{ticker}.ax', 'yahoo')
-    df.to_csv(f'data/{ticker}.csv')
-    df['moving_20'] = df['Adj Close'].rolling(window=20).mean()
-    df['moving_100'] = df['Adj Close'].rolling(window=100).mean()
-    df = df.tail(1)
-    df['ASX code'] = ticker
-    print(ticker)
-    return df
-
-@app.route("/update_latest")
-def thread_latest():
-    start = time.time()
-    pool = ThreadPool(8)
-    results = pool.map(latest_data, asx200['Code'])
-    pool.close()
-    pool.join()
+def update_corr(tickers):
+    corr_df = pd.DataFrame()
+    for ticker in tickers:
+        try:
+            corr = pd.read_csv(f'data/{ticker}.csv',  header=0, index_col=0)
+            corr['pc_change'] = (corr['Adj Close'] - corr['Open']) / abs(corr['Open']) * 100
+            corr.rename(columns={'pc_change': ticker}, inplace=True)
+            corr.drop(['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'], 1, inplace=True)
+            corr_df = corr_df.join(corr, how='outer')
+            print(ticker)
+        except Exception as e: print(e)
     
+    corr_df.to_csv('joined_closes.csv')
+    corr_df = corr_df.corr()
+    corr_df.to_csv('corr.csv')
+
+def summary(results):
     main_df = pd.DataFrame()
     main_df = main_df.append(results)
     main_df['price_change'] = main_df['Adj Close'] - main_df['Open']
     main_df['pc_change'] = (main_df['Adj Close'] - main_df['Open']) / abs(main_df['Open']) * 100
-    main_df = pd.merge(asxlist, main_df, on='ASX code')
+    main_df = pd.merge(asxlist, main_df, on='Code')
     main_df = main_df.round({'Adj Close': 3, 'price_change': 3, 'pc_change': 2})
 
     main_df.loc[main_df['Adj Close'] >= main_df['moving_20'], 'short_pred'] = 'buy'  
     main_df.loc[main_df['Adj Close'] < main_df['moving_20'], 'short_pred'] = 'sell' 
-
     main_df.loc[main_df['Adj Close'] >= main_df['moving_100'], 'long_pred'] = 'buy'  
     main_df.loc[main_df['Adj Close'] < main_df['moving_100'], 'long_pred'] = 'sell'  
 
-    main_df.set_index('ASX code', inplace=True)
+    main_df.set_index('Code', inplace=True)
     main_df.to_csv('all_latest.csv')
-    main_df = main_df.drop(['GICS industry group', 'High', 'Low', 'Open', 'Close', 'moving_20', 'moving_100'], axis=1)
+    main_df.drop(['GICS industry group', 'High', 'Low', 'Open', 'Close', 'moving_20', 'moving_100'], axis=1, inplace=True)
     main_df.to_csv('latest_data.csv')
-    print(time.time() - start)
-    return jsonify({'message':'Complete'})
+
+def latest_data(ticker):
+    #df = pd.read_csv(f'data/{ticker}.csv',  header=0)
+    try:
+        df = web.DataReader(f'{ticker}.ax', 'yahoo')
+        df.to_csv(f'data/{ticker}.csv')
+        df['moving_20'] = df['Adj Close'].rolling(window=20).mean()
+        df['moving_100'] = df['Adj Close'].rolling(window=100).mean()
+        df = df.tail(1)
+        df['Code'] = ticker
+        print(ticker)
+        return df
+    except Exception as e: print(e)
+
+@app.route("/update")
+def thread_latest():
+
+    tickers = asxlist['Code']
+
+    start = time.time()
+    pool = ThreadPool(8)
+    results = pool.map(latest_data, tickers)
+    pool.close()
+    pool.join()
+
+    summary(results)
+    update_corr(tickers)
+
+    duration = time.time() - start
+    return jsonify({'message':'Complete', 'time':duration})
 
 @app.route("/latest")
 def get_latest():
     df = pd.read_csv('latest_data.csv', header=0)
     return df.to_json(orient='records')
-
-@app.route("/update_corr")
-def update_corr():
-    main_df = pd.DataFrame()
-    for ticker in asx200['Code']:
-        df = pd.read_csv(f'data/{ticker}.csv',  header=0, index_col=0)
-        df['pc_change'] = (df['Adj Close'] - df['Open']) / abs(df['Open']) * 100
-        df.rename(columns={'pc_change': ticker}, inplace=True)
-        df.drop(['Open', 'High', 'Low', 'Close', 'Volume', 'Adj Close'], 1, inplace=True)
-        main_df = main_df.join(df, how='outer')
-        print(ticker)
-    
-    main_df.to_csv('joined_closes.csv')
-    df_corr = main_df.corr()
-    df_corr.to_csv('corr.csv')
-    return jsonify({'message':'Complete'})
-
 
 @app.route('/')
 def hello_world():
@@ -199,3 +191,23 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
+
+
+
+# def get_data(ticker):
+#     try:
+#         print(ticker)
+#         df = web.DataReader(f'{ticker}.ax', 'yahoo')
+#         df.to_csv(f'data/{ticker}.csv')
+#     except Exception as e: print(e)
+
+
+# @app.route("/update_data")
+# def thread_get():
+#     start = time.time()
+#     pool = ThreadPool(8)
+#     results = pool.map(get_data, asx200['Code'])
+#     pool.close()
+#     pool.join()
+#     print(time.time() - start)
+#     return jsonify({'message':'Complete'})
